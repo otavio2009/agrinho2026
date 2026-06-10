71f2c667b9c0a50d8b3d6827712141af10b77ccb
otavio2009 <142619830+otavio2009@users.noreply.github.com>
2026-06-10 16:17:21 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function mostrarDicas() {
+    alert(
+        "🌱 Regue as plantas todos os dias!\n" +
+        "🌞 Coloque-as no sol quando precisarem!\n" +
+        "🧹 Mantenha a terra limpa e sem lixo!\n" +
+        "😊 Cuide das plantas e ajude a natureza!"
+    );
+}
